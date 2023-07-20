f06824134b652f2d5d0caef9d2159f103f4564a4
Peter DADA <dapetoo@gmail.com>
2023-07-20 15:52:54 +0100

    Setup routes using httpRouter package

---
+++ cmd/api/routes.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func (app *application) routes() *httprouter.Router {
+	//Initialize a new httpRouter instance
+	router := httprouter.New()
+
+	//Register the methods using HandlerFun
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthCheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
+
+	return router
+}

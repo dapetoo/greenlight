15e74184e7c43fb6b84bf1a0837050ac23f6cd1c
Peter DADA <dapetoo@gmail.com>
2023-07-22 00:37:22 +0100

    Implementing error for different events and update healthcheck to use updated error check helper methods

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func (app *application) logError(r *http.Request, err error) {
 	app.logger.Println(err)
@@ -14,6 +17,26 @@ func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, st
 	err := app.writeJSON(w, status, env, nil)
 	if err != nil {
 		app.logError(r, err)
-		w.WriteHeader(http.StatusInternalServerError)
+		w.WriteHeader(500)
 	}
+}
+
+// serverErrorResponse for 500 http status code error
+func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.logError(r, err)
+
+	message := "the server encountered a problem and could not process your request"
+	app.errorResponse(w, r, http.StatusInternalServerError, message)
+}
+
+// notFoundResponse() for a 404 not found status code and JSON response to the client
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
+	message := "the requested resource could not be found"
+	app.errorResponse(w, r, http.StatusNotFound, message)
+}
+
+// methodNotAllowedResponse for a 405 status code error
+func (app *application) methodNotAllowed(w http.ResponseWriter, r *http.Request) {
+	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
+	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
 }

b8d3412cfed8712a0a949b04f04e2c4f1b86d41d
Peter DADA <dapetoo@gmail.com>
2023-07-21 03:49:29 +0100

    Helper method to avoid code duplicate for encoding JSON data

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/julienschmidt/httprouter"
@@ -16,4 +17,27 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 		return 0, errors.New(fmt.Sprintf("the %d is invalid", id))
 	}
 	return id, nil
+}
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
+	//Encode the data to JSON returning error if there was one
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	//
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, err = w.Write(js)
+	if err != nil {
+		return err
+	}
+	return nil
 }

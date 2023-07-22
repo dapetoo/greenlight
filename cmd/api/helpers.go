05a92adbe54e5ecf129be33856def6208a6fd636
Peter DADA <dapetoo@gmail.com>
2023-07-22 18:48:33 +0100

    UnmarshalJSON and MarshalJSON for Runtime type

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"net/http"
 	"strconv"
+	"strings"
 )
 
 // Define an envelop type
@@ -47,8 +48,16 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 }
 
 func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst interface{}) error {
+	//Set a limit on the size of the request body to 1MB
+	maxBytes := 1_048_576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	//Init new json.Decoder and call DisallowUnknownFields() before decoding the request body
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
 	//Decode the request body into the target destination
-	err := json.NewDecoder(r.Body).Decode(dst)
+	err := dec.Decode(dst)
 	if err != nil {
 		var syntaxError *json.SyntaxError
 		var unmarshalTypeError *json.UnmarshalTypeError
@@ -70,6 +79,15 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst int
 		case errors.Is(err, io.EOF):
 			return errors.New("body must not be empty")
 
+		//if json contains a field that cannot be map to the target destination
+		case strings.HasPrefix(err.Error(), "json: unknown field"):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field")
+			return fmt.Errorf("body contain unknown key %s", fieldName)
+
+		//Request body exceeds 1MB
+		case err.Error() == "http: request body too large":
+			return fmt.Errorf("body must not be larger thanj %d bytes", maxBytes)
+
 		case errors.As(err, &invalidUnmarshalError):
 			panic(err)
 
@@ -77,6 +95,10 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst int
 		default:
 			return err
 		}
+	}
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only contain a single JSON value")
 	}
 	return nil
 }

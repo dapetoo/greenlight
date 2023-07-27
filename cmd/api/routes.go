f6cd5616234fefed5770c864a00945df2a056d24
Peter DADA <dapetoo@gmail.com>
2023-07-27 23:37:31 +0100

    Update routes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -14,8 +14,9 @@ func (app *application) routes() *httprouter.Router {
 
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowed)
 
-	//Register the methods using HandlerFun
+	//Register the methods using HandlerFunc
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthCheckHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)

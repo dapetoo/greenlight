d79c401db7ce4f2e557794739e8807cbc2f8cbfb
Peter DADA <dapetoo@gmail.com>
2023-07-28 18:09:18 +0100

    Implement pagination

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -143,10 +143,11 @@ func (m *MovieModel) GetAll(title string, genres []string, filters Filters) ([]*
 		FROM movies
 		WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 ='' )
 		AND (genres @> $2 OR $2 = '{}')
-		ORDER BY %s %s, id ASC`, filters.sortColumn(), filters.sortDirection())
+		ORDER BY %s %s, id ASC
+		LIMIT $3 OFFSET $4`, filters.sortColumn(), filters.sortDirection())
 
 	//QueryContext to execute the query
-	rows, err := m.DB.QueryContext(ctx, query, title, pq.Array(genres))
+	rows, err := m.DB.QueryContext(ctx, query, title, pq.Array(genres), filters.limit(), filters.offset())
 	if err != nil {
 		return nil, err
 	}

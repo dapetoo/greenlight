94f6ac748d52d15c0a2063faa9440b6e588a356c
Peter DADA <dapetoo@gmail.com>
2023-07-28 20:19:22 +0100

    Setting up helper method for logging

--- internal/jsonlog/jsonlog.go
+++ internal/jsonlog/jsonlog.go
@@ -1,15 +1,18 @@
 package jsonlog
 
 import (
+	"encoding/json"
 	"io"
+	"runtime/debug"
 	"sync"
+	"time"
 )
 
 type Level int8
 
 const (
 	LevelInfo  Level = iota //Has the value of 0
-	LeverError              //Has the value of 1 etc
+	LevelError              //Has the value of 1 etc
 	LevelFatal
 	LevelOff
 )
@@ -19,7 +22,7 @@ func (l Level) String() string {
 	switch l {
 	case LevelInfo:
 		return "INFO"
-	case LeverError:
+	case LevelError:
 		return "ERROR"
 	case LevelFatal:
 		return "FATAL"
@@ -41,4 +44,51 @@ func New(out io.Writer, minLevel Level) *Logger {
 		out:      out,
 		minLevel: minLevel,
 	}
+}
+
+func (l *Logger) PrintInfo(message string, properties map[string]string) {
+	l.print()
+}
+
+// Internal method for writing the log entry
+func (l *Logger) print(level Level, message string, properties map[string]string) (int, error) {
+	//If the severity level of the log entry is below the minimum severity for the logger then return no further action
+	if level < l.minLevel {
+		return 0, nil
+	}
+
+	//Declare anonymous struct holding the data for the log entry
+	aux := struct {
+		Level      string            `json:"level"`
+		Time       string            `json:"time"`
+		Message    string            `json:"message"`
+		Properties map[string]string `json:"properties,omitempty"`
+		Trace      string            `json:"trace,omitempty"`
+	}{
+		Level:      level.String(),
+		Time:       time.Now().UTC().Format(time.RFC3339),
+		Message:    message,
+		Properties: properties,
+	}
+
+	//Include a stack trace for entries at the ERROR and FATAL levels
+	if level >= LevelError {
+		aux.Trace = string(debug.Stack())
+	}
+
+	//Declare a line variable for holding the actual log entry text
+	var line []byte
+
+	//Marshal the anonymous struct to JSON and store it in the line variable
+	line, err := json.Marshal(aux)
+	if err != nil {
+		line = []byte(LevelError.String() + ": unable to marshal log message: " + err.Error())
+	}
+
+	//Lock the mutex so that no two writes to the output destination can happen concurrently.
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	//Write the log entry
+	return l.out.Write(append(line, '\n'))
 }

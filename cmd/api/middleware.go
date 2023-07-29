8b76d4bd0c1822cc057789132b351490c875b392
Peter DADA <dapetoo@gmail.com>
2023-07-29 18:44:41 +0100

    Middleware to recover panic

---
+++ cmd/api/middleware.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				//If there's a panic, set a "Connection: close" header on the response. This automatically close the
+				//current connection after a response has been sent
+				w.Header().Set("Connection", "close")
+				app.serverErrorResponse(w, r, fmt.Errorf("%s", err))
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}

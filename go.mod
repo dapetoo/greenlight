030664fc1a9876dd6e4c8d6d294ec74fa4d60f5c
Peter DADA <dapetoo@gmail.com>
2023-07-31 01:52:14 +0100

    Implement bcrypt for password hashing

--- go.mod
+++ go.mod
@@ -6,12 +6,13 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/lib/pq v1.10.9
+	github.com/rs/zerolog v1.29.1
+	golang.org/x/crypto v0.11.0
+	golang.org/x/time v0.3.0
 )
 
 require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
-	github.com/rs/zerolog v1.29.1 // indirect
 	golang.org/x/sys v0.10.0 // indirect
-	golang.org/x/time v0.3.0 // indirect
 )
--- internal/data/users.go
+++ internal/data/users.go
@@ -1,6 +1,10 @@
 package data
 
-import "time"
+import (
+	"errors"
+	"time"
+)
+import "golang.org/x/crypto/bcrypt"
 
 //User struct to represent an individual user.
 
@@ -18,4 +22,32 @@ type User struct {
 type password struct {
 	plaintext *string
 	hash      []byte
+}
+
+// Set method calculates the bcrypt hash of a plaintext password and stores both the hash anf the plaintext versions
+// in the struct
+func (p *password) Set(plaintextPassword string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plaintextPassword), 12)
+	if err != nil {
+		return err
+	}
+	p.plaintext = &plaintextPassword
+	p.hash = hash
+	return nil
+}
+
+// Matches method checks the provided plaintext password matches the hashed password stored in the struct, returning true if
+func (p *password) Matches(plaintextPassword string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword(p.hash, []byte(plaintextPassword))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+
+		}
+	}
+	return true, nil
+
 }

9491efb94418af18db5ba8c74d34cad8dafaef94
Peter DADA <dapetoo@gmail.com>
2023-08-03 20:04:12 +0100

    Generate token function

--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -30,16 +30,16 @@ func generateToken(userID int64, ttl time.Duration, scope string) (*Token, error
 	}
 
 	//Init a zero-valued byte slice with a lenght of 16bytes
-	randowmBytes := make([]byte, 16)
+	randomBytes := make([]byte, 16)
 
 	//Read() to fill the byte slice with random bytes
-	_, err := rand.Read(randowmBytes)
+	_, err := rand.Read(randomBytes)
 	if err != nil {
 		return nil, err
 	}
 
 	//Encode the byte slice to a base-32 encoded string and assign it to the token Plaintext field.
-	token.Plaintext = base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(randowmBytes)
+	token.Plaintext = base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(randomBytes)
 
 	//Generate a SHA-256 hash of the plaintext token string. this will be stored in the hash field of the DB table
 	hash := sha256.Sum256([]byte(token.Plaintext))

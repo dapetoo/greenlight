fc1b971e26ba8d34f22bafb7e8a04044613bfccc
Peter DADA <dapetoo@gmail.com>
2023-08-04 07:19:15 +0100

    Migrations table

--- migrations/000005_create_tokens_table.up.sql
+++ migrations/000005_create_tokens_table.up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS tokens(
+CREATE TABLE IF NOT EXISTS tokens (
     hash bytea PRIMARY KEY,
     user_id bigint NOT NULL REFERENCES users ON DELETE CASCADE,
     expiry TIMESTAMP(0)  WITH time ZONE NOT NULL,
